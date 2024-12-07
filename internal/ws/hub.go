7c511eb2dd34498a76aa82d1055e29a4b210fd21
Guinn <eguinjonathan08@gmail.com>
2024-12-08 06:41:13 +0700

    Add channels for client registration, unregistration, and broadcasting in WebSocket hub

--- internal/ws/hub.go
+++ internal/ws/hub.go
@@ -4,10 +4,22 @@ import "websocket/internal/models"
 
 type Hub struct {
 	Rooms map[string]*models.ChatRoom
+	Register chan *models.Client
+	Unregister chan *models.Client
+	Broadcast chan *models.Chat
 }
 
 func NewHub() *Hub {
 	return &Hub{
 		Rooms: make(map[string]*models.ChatRoom),
+		Register: make(chan *models.Client),
+		Unregister: make(chan *models.Client),
+		Broadcast: make(chan *models.Chat),
+	}
+}
+
+func (h *Hub) Run() {
+	for _, room := range h.Rooms {
+		go room.Run()
 	}
 }
